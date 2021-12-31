bd519a97df3c6e77f15c2f1cf8aff4dad2447b67
Jon Perrett <jonperrett@hotmail.co.uk>
2021-12-31 11:55:25 +0000

    DOCUMENTATION: adds docstring for telemetry manager class

--- telemetry_manager.py
+++ telemetry_manager.py
@@ -4,6 +4,17 @@ from telemetry_f1_2021.listener import TelemetryListener
 
 
 class TelemetryManager(Thread):
+    """Class for adding packets to the packet queue.
+
+    Derived from the Thread class, this is run as part of a multithreaded program.
+
+    The class initialises a TelemetryListener object and uses this to gather packets
+    from the UDP stream. These are then added to a separate packet queue by reference.
+
+    Methods:
+        run - called as part of the start method in Thread. Gets packets and adds them to the queue.
+    """
+
     def __init__(self, packet_queue):
         Thread.__init__(self)
         self.packet_queue = packet_queue
